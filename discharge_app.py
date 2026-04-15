266d8ec3b033536a44423784d514fabde9955883
ValleyVyper <akshayatsomework@gmail.com>
2026-04-15 10:07:31 +0530

    Update discharge_app.py

--- discharge_app.py
+++ discharge_app.py
@@ -135,7 +135,7 @@ if uploaded_files:
             genai.configure(api_key=st.secrets["gemini_key"])
             
             model = genai.GenerativeModel(
-    model_name="gemini-2.5-flash",
+                model_name="gemini-2.5-flash",
                 system_instruction=SYSTEM_PROMPT,
                 generation_config={"temperature": 0.0}
             )
